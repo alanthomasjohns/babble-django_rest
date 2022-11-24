c304b10bb89a2b0f04ee8832988b9b3feec3ae97
Alan <alanthomasjohns99@gmail.com>
2022-11-24 23:11:28 +0530

    solved staticfiles

--- core/settings.py
+++ core/settings.py
@@ -179,7 +179,7 @@ import os
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
